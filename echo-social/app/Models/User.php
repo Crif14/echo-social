2ced13752c59fe391429acb669b6f3a9d8860218
Crif14 <CSqueo22@gmail.com>
2026-05-07 17:31:14 +0200

    Update User

--- echo-social/app/Models/User.php
+++ echo-social/app/Models/User.php
@@ -2,31 +2,41 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $table = 'users';
+
+    protected $fillable = ['name', 'email', 'password', 'role'];
+
+    protected $hidden = ['password', 'remember_token'];
+
+    public function posts()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Post::class, 'userId');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'userId');
+    }
+
+    public function dailyTopics()
+    {
+        return $this->hasMany(DailyTopic::class, 'userId');
+    }
+
+    public function likedPosts()
+    {
+        return $this->belongsToMany(Post::class, 'postLikes', 'userId', 'postId');
+    }
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
     }
 }
