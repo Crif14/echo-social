28199a5e56291fcb36af309d231df5a601e15882
Crif14 <CSqueo22@gmail.com>
2026-05-08 09:33:40 +0200

    Echo social -- Auto login

--- echo-social/routes/web.php
+++ echo-social/routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect()->route('posts.index');
+    }
     return view('welcome');
 });
 
